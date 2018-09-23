696363191e71bf20809da8dd7df69a058aea5448
Travis Geiselbrecht <travisg@gmail.com>
2018-09-23 16:09:45 -0700

    [boot] dump the e820 memory table, if present

--- main.c
+++ main.c
@@ -22,17 +22,36 @@
  */
 #include <stdint.h>
 
+#include <compiler.h>
 #include <console.h>
 #include <stdio.h>
 
+struct e820 {
+    uint64_t base;      // 0x0
+    uint64_t len;       // 0x8
+    uint32_t type;      // 0x10
+
+    uint8_t  pad[0xc];  // pad out to 0x20 bytes, as returned by the bootloader
+} __PACKED;
+
+static void dump_e820(const void *ptr, size_t count) {
+    const struct e820 *e820 = ptr;
+
+    for (size_t i = 0; i < count; i++) {
+        printf("e820 %zu: base %#llx len %#llx type %lu\n", i, e820[i].base, e820[i].len, e820[i].type);
+    }
+}
+
 // main C entry point
-void _start_c(unsigned int mem, void *ext_mem_block, int ext_mem_count, int in_vesa, void *vesa_ptr) {
+void _start_c(unsigned int mem, struct e820 *ext_mem_block, size_t ext_mem_count, int in_vesa, void *vesa_ptr) {
     console_init(true);
 
     printf("Welcome to 3x86 OS\n");
 
-    printf("arguments from bootloader:\n\tmem %#x\n\text_mem_block %p ext_mem_count %d\n\tin_vesa %d vesa_ptr %p\n",
+    printf("arguments from bootloader:\n\tmem %#x\n\text_mem_block %p ext_mem_count %zu\n\tin_vesa %d vesa_ptr %p\n",
             mem, ext_mem_block, ext_mem_count, in_vesa, vesa_ptr);
+
+    dump_e820(ext_mem_block, ext_mem_count);
 
     printf("Reached the end. Spinning forever\n");
     __asm__ volatile("cli");
@@ -40,3 +59,5 @@ void _start_c(unsigned int mem, void *ext_mem_block, int ext_mem_count, int in_v
         __asm__ volatile("hlt");
     }
 }
+
+
