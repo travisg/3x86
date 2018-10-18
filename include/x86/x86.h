eaa1610492a65591b75fe3f68e1e96a71a929cc3
Travis Geiselbrecht <geist@foobox.com>
2018-10-18 10:31:24 -0700

    [x86] use the N constraint for in/out instructions

--- include/x86/x86.h
+++ include/x86/x86.h
@@ -210,7 +210,7 @@ static inline uint8_t inp(uint16_t _port) {
     uint8_t rv;
     __asm__ __volatile__ ("inb %1, %0"
                           : "=a" (rv)
-                          : "di" (_port));
+                          : "dN" (_port));
     return (rv);
 }
 
@@ -218,7 +218,7 @@ static inline uint16_t inpw (uint16_t _port) {
     uint16_t rv;
     __asm__ __volatile__ ("inw %1, %0"
                           : "=a" (rv)
-                          : "di" (_port));
+                          : "dN" (_port));
     return (rv);
 }
 
@@ -226,28 +226,28 @@ static inline uint32_t inpd(uint16_t _port) {
     uint32_t rv;
     __asm__ __volatile__ ("inl %1, %0"
                           : "=a" (rv)
-                          : "di" (_port));
+                          : "dN" (_port));
     return (rv);
 }
 
 static inline void outp(uint16_t _port, uint8_t _data) {
     __asm__ __volatile__ ("outb %1, %0"
                           :
-                          : "di" (_port),
+                          : "dN" (_port),
                           "a" (_data));
 }
 
 static inline void outpw(uint16_t _port, uint16_t _data) {
     __asm__ __volatile__ ("outw %1, %0"
                           :
-                          : "di" (_port),
+                          : "dN" (_port),
                           "a" (_data));
 }
 
 static inline void outpd(uint16_t _port, uint32_t _data) {
     __asm__ __volatile__ ("outl %1, %0"
                           :
-                          : "di" (_port),
+                          : "dN" (_port),
                           "a" (_data));
 }
 
